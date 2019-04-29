379ce88fa44312cc33f4d77c82804474c6b15da6
imtiaz.a <imtiaz@uncommon.is>
2019-04-29 17:50:46 +0530

    minor fix

--- actions/add_book.php
+++ actions/add_book.php
@@ -1,15 +1,22 @@
 <?php 
-session_start();
-if(!ISSET($_SESSION['username'])){
-    header("Location:http://localhost/project_x/login.php");
-}
-$username = $_SESSION['username'];
-if (file_exists("../../users/".$username)) {
-    $file = fopen("../../users/".$username.'/books.csv', 'a');
-    $author = $_POST["author"];
-    $title = $_POST["title"];
-    fputcsv($file, array($author,$title));
-    fclose($file);
-    header("Location:http://localhost/project_x/books.php");
-}
+    session_start();
+    if(!ISSET($_SESSION['username'])){
+        header("Location:http://localhost/project_x/login.php");
+    }
+    if(!ISSET($_POST['author']) || !ISSET($_POST['title'])) {
+        header("Location:http://localhost/project_x/books.php");
+    }
+    if($_POST['author'] == "" || $_POST['title'] == "") {
+        header("Location:http://localhost/project_x/books.php");
+    } else {
+        $username = $_SESSION['username'];
+        if (file_exists("../../users/".$username)) {
+            $file = fopen("../../users/".$username.'/books.csv', 'a');
+            $author = $_POST["author"];
+            $title = $_POST["title"];
+            fputcsv($file, array($author,$title));
+            fclose($file);
+            header("Location:http://localhost/project_x/books.php");
+        }
+    }
 ?>
